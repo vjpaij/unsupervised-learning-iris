af900451348efabd420c99a8d0f383f42399eecd
Vijay Pai <vijay.pai@tesco.com>
2025-02-03 19:26:23 +0530

    k-mean cluster

--- iris.py
+++ iris.py
@@ -14,3 +14,17 @@ sns.scatterplot(data=iris_df, x='sepal_length', y='petal_length', hue='species')
 numeric_cols = iris_df.select_dtypes(include=[np.number]).columns
 X = iris_df[numeric_cols]
 
+from sklearn.cluster import KMeans
+model = KMeans(n_clusters=3, random_state=42)
+model.fit(X)
+print(model.cluster_centers_)
+preds = model.predict(X)
+print(preds)
+
+sns.scatterplot(data=X, x='sepal_length', y='petal_length', hue=preds)
+centers_x, centers_y = model.cluster_centers_[:, 0], model.cluster_centers_[:, 2]
+plt.plot(centers_x, centers_y, 'xb')
+plt.show()
+
+
+
